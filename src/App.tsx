980f585d4d7a0e368de94df699a67c68a5e19a1a
Nando <erninan@students.zhaw.ch>
2024-04-21 00:54:21 +0200

    go

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,21 @@
 import "./style/App.css";
 import DutyPage from "./pages/DutyPage";
 import { scaleRotate as Menu } from "react-burger-menu";
-import i18next from "i18next";
 import { useEffect } from "react";
-import STRINGS from "./data/strings";
+import { useTranslation } from "react-i18next";
 
 function App() {
-  useEffect(() => {
-    i18next
-      .init({
-        lng: "en",
-        debug: true,
-        resources: {
-          ...STRINGS,
-        },
-      })
-      .then(() => {
-        i18next.reloadResources();
-      });
-  }, []);
+  useEffect(() => {}, []);
+
+  const { t, i18n } = useTranslation();
 
   return (
     <>
       <Menu right pageWrapId={"page-wrap"} outerContainerId={"outer-container"}>
-        <h2>{i18next.t("burgerMenu.clock")}</h2>
-        <h2>{i18next.t("burgerMenu.dutyPlan")}</h2>
-        <h2>{i18next.t("burgerMenu.lights")}</h2>
-        <h2>{i18next.t("burgerMenu.jokeOfTheDay")}</h2>
+        <h2>{t("burgerMenu.clock")}</h2>
+        <h2>{t("burgerMenu.dutyPlan")}</h2>
+        <h2>{t("burgerMenu.lights")}</h2>
+        <h2>{t("burgerMenu.jokeOfTheDay")}</h2>
       </Menu>
       <div className="App" id="outer-container">
         <div id="page-wrap" style={{ height: "100%" }}>
---
+++ src/i18n.tsx
@@ -0,0 +1,11 @@
+import STRINGS from "./data/strings";
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+
+i18next.use(initReactI18next).init({
+  lng: "en",
+  debug: true,
+  resources: {
+    ...STRINGS,
+  },
+});
