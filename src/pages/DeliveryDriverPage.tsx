bfe17c7d80ad064ee582dd29953e10851ff41bf0
Nando <erninan@students.zhaw.ch>
2024-05-19 23:54:17 +0200

    duration

--- src/pages/DeliveryDriverPage.tsx
+++ src/pages/DeliveryDriverPage.tsx
@@ -9,7 +9,7 @@ import SlideshowImage from "../components/SlideshowImage/SlideshowImage";
 
 const DeliveryDriverPage = () => {
   const fadeProperties = {
-    duration: 5000,
+    duration: 10000,
     transitionDuration: 500,
     infinite: true,
     indicators: true,
