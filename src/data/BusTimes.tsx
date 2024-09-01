40c9de04c9bbdb37cbe590dc2bbd280b8a134bae
nandoerni <nando.erni@outlook.de>
2024-09-01 14:03:09 +0200

    it should work

--- src/data/BusTimes.tsx
+++ src/data/BusTimes.tsx
@@ -1,43 +1,36 @@
+import { BUS_TIMES_WEEKDAYS } from "./BusTimesWeekdays";
+
 export const getNextBusTime = (): Date => {
   const now = new Date();
 
   const hour = now.getHours();
-  let minutes = now.getMinutes();
+  let minute = now.getMinutes();
 
-  let bushour = BUS_TIMES.mondayToFriday[0];
+  const currentBusTimes = BUS_TIMES.mondayToFriday;
 
-  for (let i = 0; i < BUS_TIMES.mondayToFriday.length; i++) {
-    if (
-      BUS_TIMES.mondayToFriday[i].hour == hour &&
-      BUS_TIMES.mondayToFriday[i].minutes[
-        BUS_TIMES.mondayToFriday[i].minutes.length - 1
-      ] < minutes
-    ) {
-      continue;
-    }
+  let bushour = currentBusTimes[0];
+  const timeWithCurrentHour = new Date();
+  for (let index = 0; index < currentBusTimes.length; index++) {
+    const currentBusHour = currentBusTimes[index];
 
-    if (BUS_TIMES.mondayToFriday[i].hour >= hour) {
-      bushour = BUS_TIMES.mondayToFriday[i];
+    timeWithCurrentHour.setHours(currentBusHour.hour);
+    timeWithCurrentHour.setMinutes(currentBusHour.minute);
+
+    if (now < timeWithCurrentHour) {
+      bushour = currentBusHour;
       break;
     }
   }
 
-  if (bushour.hour > hour) {
-    minutes = 0;
-  }
-
-  let i = bushour.minutes.length;
-  while (bushour.minutes[--i] > minutes);
-
   const busTime = new Date();
   busTime.setHours(bushour.hour);
-  busTime.setMinutes(bushour.minutes[i + 1]);
+  busTime.setMinutes(bushour.minute);
   return busTime;
 };
 
-type BusHour = {
+export type BusHour = {
   hour: number;
-  minutes: number[];
+  minute: number;
 };
 
 type BusTimes = {
@@ -46,91 +39,8 @@ type BusTimes = {
   sundayAndHoliday: BusHour[];
 };
 
-const BUS_TIMES_MONDAY_TO_FRIDAY: BusHour[] = [
-  {
-    hour: 0,
-    minutes: [5, 20, 35],
-  },
-  {
-    hour: 5,
-    minutes: [9, 25, 40, 56],
-  },
-  {
-    hour: 6,
-    minutes: [3, 9, 17, 24, 32, 39, 47, 54],
-  },
-  {
-    hour: 7,
-    minutes: [2, 9, 17, 24, 32, 39, 47, 54],
-  },
-  {
-    hour: 8,
-    minutes: [2, 9, 17, 24, 32, 39, 47, 54],
-  },
-  {
-    hour: 9,
-    minutes: [2, 9, 17, 24, 32, 39, 47, 54],
-  },
-  {
-    hour: 10,
-    minutes: [2, 9, 17, 24, 32, 39, 47, 54],
-  },
-  {
-    hour: 11,
-    minutes: [2, 9, 16, 24, 31, 39, 46, 54],
-  },
-  {
-    hour: 12,
-    minutes: [1, 9, 16, 24, 31, 39, 46, 54],
-  },
-  {
-    hour: 13,
-    minutes: [1, 9, 16, 24, 31, 39, 46, 54],
-  },
-  {
-    hour: 14,
-    minutes: [1, 9, 16, 24, 31, 39, 46, 54],
-  },
-  {
-    hour: 15,
-    minutes: [1, 9, 16, 24, 31, 39, 46, 54],
-  },
-  {
-    hour: 16,
-    minutes: [1, 9, 16, 24, 31, 39, 46, 54],
-  },
-  {
-    hour: 17,
-    minutes: [1, 9, 16, 24, 31, 39, 46, 54],
-  },
-  {
-    hour: 18,
-    minutes: [1, 9, 16, 24, 31, 39, 46, 54],
-  },
-  {
-    hour: 19,
-    minutes: [1, 10, 18, 25, 33, 40, 48, 55],
-  },
-  {
-    hour: 20,
-    minutes: [3, 10, 18, 25, 33, 40, 48, 54],
-  },
-  {
-    hour: 21,
-    minutes: [0, 5, 12, 20, 35, 50],
-  },
-  {
-    hour: 22,
-    minutes: [5, 20, 35, 50],
-  },
-  {
-    hour: 23,
-    minutes: [5, 20, 35],
-  },
-];
-
 const BUS_TIMES: BusTimes = {
-  mondayToFriday: BUS_TIMES_MONDAY_TO_FRIDAY,
-  saturday: BUS_TIMES_MONDAY_TO_FRIDAY,
-  sundayAndHoliday: BUS_TIMES_MONDAY_TO_FRIDAY,
+  mondayToFriday: BUS_TIMES_WEEKDAYS,
+  saturday: BUS_TIMES_WEEKDAYS,
+  sundayAndHoliday: BUS_TIMES_WEEKDAYS,
 };
---
+++ src/data/BusTimesWeekdays.tsx
@@ -0,0 +1,564 @@
+import { BusHour } from "./BusTimes";
+
+export const BUS_TIMES_WEEKDAYS: BusHour[] = [
+  {
+    hour: 0,
+    minute: 5,
+  },
+  {
+    hour: 0,
+    minute: 20,
+  },
+  {
+    hour: 0,
+    minute: 35,
+  },
+  {
+    hour: 5,
+    minute: 9,
+  },
+  {
+    hour: 5,
+    minute: 25,
+  },
+  {
+    hour: 5,
+    minute: 40,
+  },
+  {
+    hour: 5,
+    minute: 56,
+  },
+  {
+    hour: 6,
+    minute: 3,
+  },
+  {
+    hour: 6,
+    minute: 9,
+  },
+  {
+    hour: 6,
+    minute: 17,
+  },
+  {
+    hour: 6,
+    minute: 24,
+  },
+  {
+    hour: 6,
+    minute: 32,
+  },
+  {
+    hour: 6,
+    minute: 39,
+  },
+  {
+    hour: 6,
+    minute: 47,
+  },
+  {
+    hour: 6,
+    minute: 54,
+  },
+  {
+    hour: 7,
+    minute: 2,
+  },
+  {
+    hour: 7,
+    minute: 9,
+  },
+  {
+    hour: 7,
+    minute: 17,
+  },
+  {
+    hour: 7,
+    minute: 24,
+  },
+  {
+    hour: 7,
+    minute: 32,
+  },
+  {
+    hour: 7,
+    minute: 39,
+  },
+  {
+    hour: 7,
+    minute: 47,
+  },
+  {
+    hour: 7,
+    minute: 54,
+  },
+  {
+    hour: 8,
+    minute: 2,
+  },
+  {
+    hour: 8,
+    minute: 9,
+  },
+  {
+    hour: 8,
+    minute: 17,
+  },
+  {
+    hour: 8,
+    minute: 24,
+  },
+  {
+    hour: 8,
+    minute: 32,
+  },
+  {
+    hour: 8,
+    minute: 39,
+  },
+  {
+    hour: 8,
+    minute: 47,
+  },
+  {
+    hour: 8,
+    minute: 54,
+  },
+  {
+    hour: 9,
+    minute: 2,
+  },
+  {
+    hour: 9,
+    minute: 9,
+  },
+  {
+    hour: 9,
+    minute: 17,
+  },
+  {
+    hour: 9,
+    minute: 24,
+  },
+  {
+    hour: 9,
+    minute: 32,
+  },
+  {
+    hour: 9,
+    minute: 39,
+  },
+  {
+    hour: 9,
+    minute: 47,
+  },
+  {
+    hour: 9,
+    minute: 54,
+  },
+  {
+    hour: 10,
+    minute: 2,
+  },
+  {
+    hour: 10,
+    minute: 9,
+  },
+  {
+    hour: 10,
+    minute: 17,
+  },
+  {
+    hour: 10,
+    minute: 24,
+  },
+  {
+    hour: 10,
+    minute: 32,
+  },
+  {
+    hour: 10,
+    minute: 39,
+  },
+  {
+    hour: 10,
+    minute: 47,
+  },
+  {
+    hour: 10,
+    minute: 54,
+  },
+  {
+    hour: 11,
+    minute: 2,
+  },
+  {
+    hour: 11,
+    minute: 9,
+  },
+  {
+    hour: 11,
+    minute: 16,
+  },
+  {
+    hour: 11,
+    minute: 24,
+  },
+  {
+    hour: 11,
+    minute: 31,
+  },
+  {
+    hour: 11,
+    minute: 39,
+  },
+  {
+    hour: 11,
+    minute: 46,
+  },
+  {
+    hour: 11,
+    minute: 54,
+  },
+  {
+    hour: 12,
+    minute: 1,
+  },
+  {
+    hour: 12,
+    minute: 9,
+  },
+  {
+    hour: 12,
+    minute: 16,
+  },
+  {
+    hour: 12,
+    minute: 24,
+  },
+  {
+    hour: 12,
+    minute: 31,
+  },
+  {
+    hour: 12,
+    minute: 39,
+  },
+  {
+    hour: 12,
+    minute: 46,
+  },
+  {
+    hour: 12,
+    minute: 54,
+  },
+  {
+    hour: 13,
+    minute: 1,
+  },
+  {
+    hour: 13,
+    minute: 9,
+  },
+  {
+    hour: 13,
+    minute: 16,
+  },
+  {
+    hour: 13,
+    minute: 24,
+  },
+  {
+    hour: 13,
+    minute: 31,
+  },
+  {
+    hour: 13,
+    minute: 39,
+  },
+  {
+    hour: 13,
+    minute: 46,
+  },
+  {
+    hour: 13,
+    minute: 54,
+  },
+  {
+    hour: 14,
+    minute: 1,
+  },
+  {
+    hour: 14,
+    minute: 9,
+  },
+  {
+    hour: 14,
+    minute: 16,
+  },
+  {
+    hour: 14,
+    minute: 24,
+  },
+  {
+    hour: 14,
+    minute: 31,
+  },
+  {
+    hour: 14,
+    minute: 39,
+  },
+  {
+    hour: 14,
+    minute: 46,
+  },
+  {
+    hour: 14,
+    minute: 54,
+  },
+  {
+    hour: 15,
+    minute: 1,
+  },
+  {
+    hour: 15,
+    minute: 9,
+  },
+  {
+    hour: 15,
+    minute: 16,
+  },
+  {
+    hour: 15,
+    minute: 24,
+  },
+  {
+    hour: 15,
+    minute: 31,
+  },
+  {
+    hour: 15,
+    minute: 39,
+  },
+  {
+    hour: 15,
+    minute: 46,
+  },
+  {
+    hour: 15,
+    minute: 54,
+  },
+  {
+    hour: 16,
+    minute: 1,
+  },
+  {
+    hour: 16,
+    minute: 9,
+  },
+  {
+    hour: 16,
+    minute: 16,
+  },
+  {
+    hour: 16,
+    minute: 24,
+  },
+  {
+    hour: 16,
+    minute: 31,
+  },
+  {
+    hour: 16,
+    minute: 39,
+  },
+  {
+    hour: 16,
+    minute: 46,
+  },
+  {
+    hour: 16,
+    minute: 54,
+  },
+  {
+    hour: 17,
+    minute: 1,
+  },
+  {
+    hour: 17,
+    minute: 9,
+  },
+  {
+    hour: 17,
+    minute: 16,
+  },
+  {
+    hour: 17,
+    minute: 24,
+  },
+  {
+    hour: 17,
+    minute: 31,
+  },
+  {
+    hour: 17,
+    minute: 39,
+  },
+  {
+    hour: 17,
+    minute: 46,
+  },
+  {
+    hour: 17,
+    minute: 54,
+  },
+  {
+    hour: 18,
+    minute: 1,
+  },
+  {
+    hour: 18,
+    minute: 9,
+  },
+  {
+    hour: 18,
+    minute: 16,
+  },
+  {
+    hour: 18,
+    minute: 24,
+  },
+  {
+    hour: 18,
+    minute: 31,
+  },
+  {
+    hour: 18,
+    minute: 39,
+  },
+  {
+    hour: 18,
+    minute: 46,
+  },
+  {
+    hour: 18,
+    minute: 54,
+  },
+  {
+    hour: 19,
+    minute: 1,
+  },
+  {
+    hour: 19,
+    minute: 10,
+  },
+  {
+    hour: 19,
+    minute: 18,
+  },
+  {
+    hour: 19,
+    minute: 25,
+  },
+  {
+    hour: 19,
+    minute: 33,
+  },
+  {
+    hour: 19,
+    minute: 40,
+  },
+  {
+    hour: 19,
+    minute: 48,
+  },
+  {
+    hour: 19,
+    minute: 55,
+  },
+  {
+    hour: 20,
+    minute: 3,
+  },
+  {
+    hour: 20,
+    minute: 10,
+  },
+  {
+    hour: 20,
+    minute: 18,
+  },
+  {
+    hour: 20,
+    minute: 25,
+  },
+  {
+    hour: 20,
+    minute: 33,
+  },
+  {
+    hour: 20,
+    minute: 40,
+  },
+  {
+    hour: 20,
+    minute: 48,
+  },
+  {
+    hour: 20,
+    minute: 54,
+  },
+  {
+    hour: 21,
+    minute: 0,
+  },
+  {
+    hour: 21,
+    minute: 5,
+  },
+  {
+    hour: 21,
+    minute: 12,
+  },
+  {
+    hour: 21,
+    minute: 20,
+  },
+  {
+    hour: 21,
+    minute: 35,
+  },
+  {
+    hour: 21,
+    minute: 50,
+  },
+  {
+    hour: 22,
+    minute: 5,
+  },
+  {
+    hour: 22,
+    minute: 20,
+  },
+  {
+    hour: 22,
+    minute: 35,
+  },
+  {
+    hour: 22,
+    minute: 50,
+  },
+  {
+    hour: 23,
+    minute: 5,
+  },
+  {
+    hour: 23,
+    minute: 20,
+  },
+  {
+    hour: 23,
+    minute: 35,
+  },
+];
