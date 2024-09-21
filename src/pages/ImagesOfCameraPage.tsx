db0b0e69680373a4c7c80b90e8b29bbe55b82bf3
nandoerni <nando.erni@outlook.de>
2024-09-21 19:35:19 +0200

    spinner

--- src/pages/ImagesOfCameraPage.tsx
+++ src/pages/ImagesOfCameraPage.tsx
@@ -1,10 +1,26 @@
-import { Slide } from "react-slideshow-image";
 import { FlexSpreadList } from "../components/Helper/HelperComponents";
-import { Image, ImageContainer } from "../components/Webcam/Components";
 import ImageGallery from "react-image-gallery";
 import { GetImages } from "../data/FirebaseConnection";
-import { useEffect, useState } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 import "react-image-gallery/styles/css/image-gallery.css";
+import ClipLoader from "react-spinners/ClipLoader";
+import { ClimbingBoxLoader, PacmanLoader } from "react-spinners";
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "auto",
+  borderColor: "white",
+};
+
+const fadeProperties = {
+  duration: 10000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+
+  arrows: false,
+  canSwipe: true,
+};
 
 const ImagesOfCameraPage = () => {
   const [images, setImages]: any = useState([]);
@@ -12,16 +28,6 @@ const ImagesOfCameraPage = () => {
   useEffect(() => {
     GetImages().then((imgs) => setImages(imgs));
   }, []);
-
-  const fadeProperties = {
-    duration: 10000,
-    transitionDuration: 500,
-    infinite: true,
-    indicators: true,
-
-    arrows: false,
-    canSwipe: true,
-  };
 
   const getImageData = () => {
     return images.map((img: string, i: number) => ({
@@ -31,21 +37,29 @@ const ImagesOfCameraPage = () => {
   };
 
   return (
-    <FlexSpreadList key={images.length}>
-      <ImageGallery
-        showFullscreenButton={false}
-        showPlayButton={false}
-        disableSwipe={false}
-        items={getImageData()}
-      />
-      {/* <Slide {...fadeProperties}>
-        {images.map((img: string, i: number) => (
-          <ImageContainer key={i}>
-            <Image src={img} />
-          </ImageContainer>
-        ))}
-      </Slide> */}
-    </FlexSpreadList>
+    <>
+      {images.length == 0 ? (
+        <FlexSpreadList>
+          <PacmanLoader
+            color={"#ffffff"}
+            loading={true}
+            cssOverride={override}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </FlexSpreadList>
+      ) : (
+        <FlexSpreadList key={images.length}>
+          <ImageGallery
+            showFullscreenButton={false}
+            showPlayButton={false}
+            disableSwipe={false}
+            items={getImageData()}
+          />
+        </FlexSpreadList>
+      )}
+    </>
   );
 };
 
