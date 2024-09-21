67039777f150b70e83e2d3c3d1b9e3be3c6bf943
nandoerni <nando.erni@outlook.de>
2024-09-21 19:10:18 +0200

    go!

--- src/pages/ImagesOfCameraPage.tsx
+++ src/pages/ImagesOfCameraPage.tsx
@@ -1,8 +1,11 @@
 import { Slide } from "react-slideshow-image";
 import { FlexSpreadList } from "../components/Helper/HelperComponents";
 import { Image, ImageContainer } from "../components/Webcam/Components";
+import ImageGallery from "react-image-gallery";
 import { GetImages } from "../data/FirebaseConnection";
 import { useEffect, useState } from "react";
+import "react-image-gallery/styles/css/image-gallery.css";
+
 const ImagesOfCameraPage = () => {
   const [images, setImages]: any = useState([]);
 
@@ -20,15 +23,28 @@ const ImagesOfCameraPage = () => {
     canSwipe: true,
   };
 
+  const getImageData = () => {
+    return images.map((img: string, i: number) => ({
+      original: img,
+      thumbnail: img,
+    }));
+  };
+
   return (
     <FlexSpreadList key={images.length}>
-      <Slide {...fadeProperties}>
+      <ImageGallery
+        showFullscreenButton={false}
+        showPlayButton={false}
+        disableSwipe={false}
+        items={getImageData()}
+      />
+      {/* <Slide {...fadeProperties}>
         {images.map((img: string, i: number) => (
           <ImageContainer key={i}>
             <Image src={img} />
           </ImageContainer>
         ))}
-      </Slide>
+      </Slide> */}
     </FlexSpreadList>
   );
 };
